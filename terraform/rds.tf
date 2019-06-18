d1965e5e4dd5d33e4ee19fe662d176fa5d0cdf7e
Rafal Wilinski <raf.wilinski@gmail.com>
2019-06-18 12:12:26 +0200

    Add RDS

---
+++ terraform/rds.tf
@@ -0,0 +1,16 @@
+resource "random_string" "password" {
+  length = 16
+  special = false
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "10.6"
+  instance_class       = "db.t2.micro"
+  name                 = "Database"
+  username             = "${var.name}"
+  password             = "${random_string.password.result}"
+  parameter_group_name = "default.postgres5.7"
+}
