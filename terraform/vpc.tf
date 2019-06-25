a2c6b3f99788886b6554058640f9089c118b1fae
Rafal Wilinski <raf.wilinski@gmail.com>
2019-06-25 14:48:54 +0200

    Add missing public RT

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -50,3 +50,18 @@ resource "aws_route_table_association" "private" {
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(aws_route_table.private.*.id, count.index)
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = var.az_count
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
