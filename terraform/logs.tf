a6d179f48c1711758bf622e4dc162d74f2906dbf
Rafal Wilinski <raf.wilinski@gmail.com>
2019-06-25 14:48:36 +0200

    Add cloudwatch log stream

--- terraform/logs.tf
+++ terraform/logs.tf
@@ -5,3 +5,8 @@ resource "aws_cloudwatch_log_group" "task_logs" {
     Application = "${var.name}"
   }
 }
+
+resource "aws_cloudwatch_log_stream" "task_stream" {
+  name           = "${var.name}"
+  log_group_name = "${aws_cloudwatch_log_group.task_logs.name}"
+}
